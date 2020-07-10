5f929b1db7a01f18fa20c94d0ed2e1d88d018d35
abhishek <abhishek96890@gmail.com>
2020-07-10 20:55:16 +0530

    ADDED:Earlier

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -1,17 +1,22 @@
 #!/bin/bash  
-read -p "Enter the no of flips of coin:" flip
 Head=0
 Tail=0
-while [ $Head -ne $flip ] && [ $Tail -ne $flip ]
+while [ $Head -ne 21 ] && [ $Tail -ne 21 ]
 do
 Flip=$(( RANDOM%2 ))
-	if [ $Flip -eq 1 ]
-	then
-		Head=$(($Head+1))
-	else
-		Tail=$(($Tail+1))
-	fi
-
+        if [ $Flip -eq 1 ]
+        then
+                Head=$(($Head+1))
+        else
+                Tail=$(($Tail+1))
+        fi
 done
-echo "Head is Won :$Head times"
-echo "Tail is Won :$Tail times"
+if [ $Head -gt $Tail ]
+then
+	echo "Head Wons by $(($Head-$Tail)):"
+elif [ $Head -lt $Tail ]
+then
+	echo "Tail Wons by $(($Tail-$Head)):"
+else
+	echo "TIE:"
+fi
